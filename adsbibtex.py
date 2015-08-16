c0bbd17a7a26598050a0c92becf51099e975d48e
Ryan Varley <ryanjvarley@gmail.com>
2015-08-16 01:37:20 +0100

    yaml and bibcode line parser

--- adsbibtex.py
+++ adsbibtex.py
@@ -1,10 +1,15 @@
 """ Main file
 """
 
+import yaml
+import re
+
 
 def run_adsbibtex(config_file):
     config_document = load_config_file(config_file)
     yaml_front_matter, bibcode_lines = parse_config_file(config_document)
+
+    config = parse_yaml_front_matter(yaml_front_matter)
     print yaml_front_matter, bibcode_lines
 
 
@@ -32,21 +37,100 @@ def parse_config_file(config_document):
     """
 
     yaml_front_matter = []
-    i=0
+    i = 0
+    yaml_front_matter_found = False
     for i, line in enumerate(config_document):
         if line.startswith('---'):
+            yaml_front_matter_found = True
             break
         else:
             yaml_front_matter.append(line)
 
-    if not yaml_front_matter:
-        raise ADSBibtexBaseException("YAML Front matter not found")
+    if not yaml_front_matter_found:
+        raise ADSBibtexConfigError("YAML Front matter not found")
 
     yaml_front_matter = ''.join(yaml_front_matter)
 
     bibcode_lines = config_document[i + 1:]
 
     return yaml_front_matter, bibcode_lines
+
+
+def parse_yaml_front_matter(yaml_front_matter):
+    """ Parses the YAML front matter and checks for essential config params
+
+    :param yaml_front_matter:
+    :return: config
+    :rtype: dict
+    """
+
+    config = yaml.load(yaml_front_matter)
+
+    # TODO check essential config params / set defaults
+
+    return config
+
+
+def parse_bibcode_lines(bibcode_lines):
+    """ Cleans up the bibcode line list, removing comments and returning a dict of "cite_name": {"bibcode": bibcode} pairs
+
+    :param bibcode_lines: lists of bibcode lines
+    :return:
+    """
+
+    bibcode_dict = {}
+
+    for bibcode_line in bibcode_lines:
+        try:
+            cite_name, bibcode = parse_bibcode_line(bibcode_line)
+            bibcode_dict[cite_name] = {'bibcode': bibcode}
+        except TypeError:  # None was returned
+            pass
+
+    return bibcode_dict
+
+
+def parse_bibcode_line(bibcode_line):
+    """ Parses a single bibcode line into cite_name, bibcode
+
+    :param bibcode_line: a line containing bibcode [cite_name] [# comment]
+    :return: cite_name, bibcode
+    """
+    # TODO (ryan) think about replacing with regex
+    if not is_comment(bibcode_line):  # line is just a comment
+        bibcode_line = bibcode_line.replace('\t', ' ')
+        bibcode_line = bibcode_line.replace('\n', '')
+        bibcode_line = ' '.join(bibcode_line.split())
+        # replace multiple whitespace with single space
+
+        bibcode_split = bibcode_line.split(' ', 2)
+        num_splits = len(bibcode_split)
+        if num_splits == 1:
+            bibcode = cite_name = bibcode_split[0]
+        elif num_splits > 1:
+            bibcode, cite_name = bibcode_split[:2]
+            if is_comment(cite_name):
+                cite_name = bibcode
+        else:
+            return None
+
+        if bibcode and cite_name:
+            return cite_name, bibcode
+        else:
+            return None
+    else:
+        return None
+
+
+def is_comment(s):
+    """ Checks if a string looks like a comment line (first non whitespace char is #)
+    :param s: string to check
+    :return: True/False
+    """
+    if re.match(r'(\s+|)#(.*?)', s):
+        return True
+    else:
+        return False
 
 
 class ADSBibtexBaseException(Exception):
--- tests/test_adsbibtex.py
+++ tests/test_adsbibtex.py
@@ -1,18 +1,20 @@
 import unittest
 
+from hypothesis import given, assume
+from hypothesis.strategies import text
+
 import adsbibtex
 
 
-class TestParseConfigFile(unittest.TestCase):
-
+class Test_parse_config_file(unittest.TestCase):
     def test_parses_correct_file(self):
         document_list = ["# YAML front matter\n",
-                         "cache_length: 24              # hours, 3d = 72, 1w=168, 2w=336\n",
-                         "cache_db:     adsbibtex.cache # location to store cached entries\n",
-                         "bibtex_file:  test.tex        # location to output bibtex file\n",
+                         "cache_length: 24",
+                         "cache_db:     adsbibtex.cache",
+                         "bibtex_file:  test.tex",
                          "---\n",
                          "# Bibcode Name # Comment\n",
-                         "2008Natur.452..329S Swain2008   # The presence of methane in the atmosphere of an extrasolar planet\n",
+                         "2008Natur.452..329S Swain2008",
                          "2006AGUSM.A21A..06T # no name\n",
                          ]
 
@@ -23,3 +25,119 @@ class TestParseConfigFile(unittest.TestCase):
 
         self.assertEqual(yaml_front_matter, yaml_front_matter_answer)
         self.assertItemsEqual(bibcode_lines, bibcode_lines_answer)
+
+    def test_raises_ADSBibtexConfigError_if_yaml_missing(self):
+        document_list = ["cache_length: 24", "2006AGUSM.A21A..06T # no name\n"]
+
+        with self.assertRaises(adsbibtex.ADSBibtexConfigError):
+            adsbibtex.parse_config_file(document_list)
+
+    def test_raises_ADSBibtexConfigError_if_empty(self):
+        document_list = []
+
+        with self.assertRaises(adsbibtex.ADSBibtexConfigError):
+            adsbibtex.parse_config_file(document_list)
+
+
+class Test_parse_bibcode_lines(unittest.TestCase):
+
+    def test_parses_correctly(self):
+        bibcode_list = ["# Bibcode Name # Comment\n",
+                        "2008Natur.452..329S Swain2008",
+                        "2006AGUSM.A21A..06T # no name\n",
+                        "    # indented comment",
+                        "    2013ApJ...766....7W",
+                        "2015ExA...tmp....5V  Varley2015 # hi"  # double spaced gap
+                        ]
+
+        bibcode_dict = adsbibtex.parse_bibcode_lines(bibcode_list)
+
+        bibcode_dict_answer = {'Swain2008': {'bibcode': '2008Natur.452..329S'},
+                               '2006AGUSM.A21A..06T': {'bibcode': '2006AGUSM.A21A..06T'},
+                               '2013ApJ...766....7W': {'bibcode': '2013ApJ...766....7W'},
+                               'Varley2015': {'bibcode': '2015ExA...tmp....5V'},
+                               }
+
+        self.assertEqual(bibcode_dict, bibcode_dict_answer)
+
+
+class Test_parse_bibcode_line(unittest.TestCase):
+
+    def test_comment_line_returns_None(self):
+        bibcode_line = "# comment\n"
+                        # "2008Natur.452..329S Swain2008",
+                        # "2006AGUSM.A21A..06T # no name\n",
+                        # "    # indented comment",
+                        # "    2013ApJ...766....7W",
+                        # "2015ExA...tmp....5V  Varley2015 # hi"
+
+        result = adsbibtex.parse_bibcode_line(bibcode_line)
+        self.assertTrue(result is None)
+
+    def test_space_indented_comment_line_returns_None(self):
+        bibcode_line = "    # spam\n"
+        result = adsbibtex.parse_bibcode_line(bibcode_line)
+        self.assertTrue(result is None)
+
+    def test_tab_indented_comment_line_returns_None(self):
+        bibcode_line = "    # eggs\n"
+        result = adsbibtex.parse_bibcode_line(bibcode_line)
+        self.assertTrue(result is None)
+
+    def test_single_bibcode_works(self):
+        bibcode_line = "2008Natur.452..329S\n"
+        cite_name, bibcode = adsbibtex.parse_bibcode_line(bibcode_line)
+        self.assertEqual(cite_name, '2008Natur.452..329S')
+        self.assertEqual(bibcode, '2008Natur.452..329S')
+
+    def test_single_bibcode_with_comment_works(self):
+        bibcode_line = "2008Natur.452..329S # spam"
+        cite_name, bibcode = adsbibtex.parse_bibcode_line(bibcode_line)
+        self.assertEqual(cite_name, '2008Natur.452..329S')
+        self.assertEqual(bibcode, '2008Natur.452..329S')
+
+    def test_bibcode_citename_works(self):
+        bibcode_line = "2008Natur.452..329S Swain2008"
+        cite_name, bibcode = adsbibtex.parse_bibcode_line(bibcode_line)
+        self.assertEqual(cite_name, 'Swain2008')
+        self.assertEqual(bibcode, '2008Natur.452..329S')
+
+    def test_bibcode_citename_with_comment_works(self):
+        bibcode_line = "2008Natur.452..329S Swain2008 # spam"
+        cite_name, bibcode = adsbibtex.parse_bibcode_line(bibcode_line)
+        self.assertEqual(cite_name, 'Swain2008')
+        self.assertEqual(bibcode, '2008Natur.452..329S')
+
+    def test_bibcode_citename_double_spaced(self):
+        bibcode_line = "2008Natur.452..329S  Swain2008"
+        cite_name, bibcode = adsbibtex.parse_bibcode_line(bibcode_line)
+        self.assertEqual(cite_name, 'Swain2008')
+        self.assertEqual(bibcode, '2008Natur.452..329S')
+
+
+class Test_is_comment(unittest.TestCase):
+    def test_normal_comment_with_space(self):
+        self.assertTrue(adsbibtex.is_comment('# spam'))
+
+    def test_normal_comment_without_space(self):
+        self.assertTrue(adsbibtex.is_comment('#spam'))
+
+    def test_space_indented_comment(self):
+        self.assertTrue(adsbibtex.is_comment(' # spam'))
+
+    def test_space_multiple_indented_comment(self):
+        self.assertTrue(adsbibtex.is_comment("    # spam\n"))
+
+    def test_tab_indented_comment(self):
+        self.assertTrue(adsbibtex.is_comment('\t# spam'))
+
+    def test_string_with_comment_is_false(self):
+        self.assertFalse(adsbibtex.is_comment('spam # eggs'))
+
+    def test_string_with_comment_no_space_is_false(self):
+        self.assertFalse(adsbibtex.is_comment('spam#eggs'))
+
+    @given(text())
+    def test_normal_string(self, s):
+        assume('#' not in s)
+        self.assertFalse(adsbibtex.is_comment(s))
